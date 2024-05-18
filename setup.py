bfd567a668670bae5266827da7f9834eda25a2b5
HoAnhKhoa <traincolab2131999v1@gmail.com>
2024-05-18 17:32:17 +0700

    Add code for build python as package

---
+++ setup.py
@@ -0,0 +1,8 @@
+# Src: https://youtu.be/Mgp6-ZMEcE0?si=ihkm3YejaIOKiUlg
+from setuptools import setup, find_packages
+
+setup(
+    name = "danh_product",
+    version= "1.0",
+    packages= find_packages()
+)
